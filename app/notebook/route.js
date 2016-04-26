07eec836ded459e7d6d6fc43d9c48c70dcfacf55
Sohjiro <felipe@makingdevs.com>
2016-04-26 16:50:52 -0500

    adding add new action

--- app/notebook/route.js
+++ app/notebook/route.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    addNew: function() {
+      var notebook = this.store.createRecord('notebook', {
+        subject: this.controller.get('subject')
+      });
+
+      notebook.save().then(() => {
+        console.log('save succesful');
+        this.controller.set('message', `A new notebook with the name ${this.controller.get('subject')} was added!`);
+        this.controller.set('name', null);
+      }, function() {
+        console.log('save failed');
+      });
+    }
+  }
 });
